fbd39dec9249cfd1d21e20d83b87c98df1e20eca
Uni <129289821+UniDCYT@users.noreply.github.com>
2024-08-25 16:33:17 +0200

    Add files via upload

---
+++ obf_55pzH3tHqpJS6048eOl16iG4C7S9k5TRupX3HRc4G38I85kP6ICDp8tmlK6954S8 (1).lua
@@ -0,0 +1,25 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.7) ~  Much Love, Ferib 
+
+]]--
+
+_G.auto = true;
+while _G.auto do
+	local ReplicatedStorage = game:GetService("ReplicatedStorage");
+	local HackEvent = ReplicatedStorage:WaitForChild("HackEvent");
+	HackEvent:FireServer();
+	wait(0.001);
+end
+_G.auto = true;
+while _G.auto do
+	local ReplicatedStorage = game:GetService("ReplicatedStorage");
+	local HackEvent = ReplicatedStorage:WaitForChild("Hack1Event");
+	HackEvent:FireServer();
+	wait(0.001);
+end
